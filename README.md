2442e9ec886363630ac753484f6cc7b99038ab9c
humbl3LilaS <sabishinekobebe@gmail.com>
2024-11-11 23:40:55 +0630

    add commands

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practice-cli
+
+> Simple cli project using node and typecript 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
 #!/user/bin/env node
 
-console.log("type script")
+import {Command} from "commander";
+import * as process from "node:process";
+
+// Declare Program
+const program = new Command();
+
+
+// Add actions to the cli
+program
+    .argument("<string>", "string to log")
+    .option("-C, --capitalize", "Capitalize message")
+    .action((message: string, opts: { capitalize?: boolean }) => {
+        if(opts.capitalize) {
+            console.log(`Commander Said ${message.toUpperCase()}`);
+        } else {
+            console.log(`Commander Said ${message}`);
+        }
+    })
+    .description("Commander Said <message>")
+
+program.command("add <numbers...>").action((numbers: string[]) => {
+    const total = numbers.reduce((a, b) => a + parseInt(b), 0);
+    console.log(`Total: ${total}`);
+}).description("Add the numbers")
+
+
+program.command("get-max <numbers...>").action((numbers: string[]) => {
+    const max = numbers.reduce((a, b) => {
+        if (a === parseInt(b)) {
+            return a
+        }
+        return a > parseInt(b) ? a : parseInt(b)
+    }, 0);
+    console.log(`Max: ${max}`);
+}).description("Get the max number")
+// Execute the cli
+program.parse(process.argv);
